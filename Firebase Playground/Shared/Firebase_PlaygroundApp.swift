affe9022fce2cf4ac86210debbc180cf54aaf8d8
Shane Cowherd <shane@cowherd.com>
2020-12-09 21:06:36 -0700

    Initial Commit

---
+++ Firebase Playground/Shared/Firebase_PlaygroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  Firebase_PlaygroundApp.swift
+//  Shared
+//
+//  Created by Shane Cowherd on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct Firebase_PlaygroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
