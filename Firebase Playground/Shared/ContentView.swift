4b60451984151655b0b256a8e0bac73c4481cba7
Shane Cowherd <shane@cowherd.com>
2020-12-09 21:34:48 -0700

    Initial Firebase createEntry function

--- Firebase Playground/Shared/ContentView.swift
+++ Firebase Playground/Shared/ContentView.swift
@@ -6,12 +6,45 @@
 //
 
 import SwiftUI
+import Firebase
+import FirebaseFirestore
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, world!")
+                .padding()
+            Button("Create Entry") {
+                createEntry()
+            }
+        }
+        
     }
+    
+    
+    func createEntry() {
+        
+        Firestore.firestore().collection("books").addDocument(data: ["Book1": "Pages1"])
+        
+//        Firestore.firestore().collection("books").addSnapshotListener { (querySnapshot, error) in
+//          guard let documents = querySnapshot?.documents else {
+//            print("No documents")
+//            return
+//          }
+//
+//          documents.map { queryDocumentSnapshot -> Book in
+//            // map document to Book instance here
+//            return Book(title: "Post PC", author: "Shane Cowherd", numberOfPages: 8)
+//          }
+//        }
+    }
+}
+
+struct Book: Identifiable {
+  var id: String = UUID().uuidString
+  var title: String
+  var author: String
+  var numberOfPages: Int
 }
 
 struct ContentView_Previews: PreviewProvider {
--- Firebase Playground/Shared/Firebase_PlaygroundApp.swift
+++ Firebase Playground/Shared/Firebase_PlaygroundApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct Firebase_PlaygroundApp: App {
+    
+    init() {
+      FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
